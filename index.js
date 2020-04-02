5f469b4705535e12cefd23f2c68d4b67430de4c4
sho-hama <shokihamana@gmail.com>
2020-04-02 23:17:28 +0900

    githubに上げとく用に調整

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const basic = auth.basic({
     file: './password'
 });
 
+//勤怠履歴
 var history = [];
+
 const server = http.createServer(basic.check((req, res) => {
     switch (req.url) {
     case '/top':
@@ -28,14 +30,14 @@ const server = http.createServer(basic.check((req, res) => {
         let data = [];
         if(req.method === 'POST'){
             req.on('data', (chunk) => {
-                res.writeHead(303, {
-                    'Location': '/top'
-                });
                 data.push(chunk);
             }).on('end', () => {
                 data = data.toString();
                 const decoded = decodeURIComponent(data);
                 let kintai = decoded ? decoded.split('kintai-button=')[1] : '';
+                res.writeHead(303, {
+                    'Location': '/top'
+                });
                 kintai = req.user + ':' + kintai
                 history.unshift(kintai);
                 res.end();
@@ -44,29 +46,14 @@ const server = http.createServer(basic.check((req, res) => {
         else{
             handleBadRequest(req, res);
         }
+    case '/old-url' :
+        handleMovedPermanently(req, res);
         break;
-    case '/logout':
-        res.writeHead(401, {
-            'Content-Type': 'text/html; charset=utf-8'
-        });
-        res.end('<!DOCTYPE html><html lang="jp"><body>' +
-                '<h1>ログアウトしました</h1>' +
-                '<a href="/top">ログイン</h1>' +
-                '</body></html>');
-    case '/old-url':
-          res.writeHead(301, {
-            'Content-Type': 'text/html; charset=utf-8'
-        });
-        res.end('<!DOCTYPE html><html lang="jp"><body>' +
-                '<h1>新しいURLに移動しました</h1>' +
-                '<a href="/top">新しいほう</h1>' +
-                '</body></html>');
+    case '/logout' :
+        handleUnauthorized(req, res);
+        break;
     default:
-        res.writeHead(404, {
-            'Content-Type': 'text/plain; charset=utf-8'
-        });
-        const message = "指定したURLが見つかりません。";
-        res.end('status code :' + res.statusCode + " " +message);
+        handleNotFound(req, res);
         break;
     }
 }));
@@ -77,6 +64,35 @@ function handleBadRequest(req, res) {
         'Content-Type': 'text/plain; charset=utf-8'
     });
     const message = "未対応のリクエストです。";
+    res.end('status code :' + res.statusCode + " " + message);
+}
+
+function handleMovedPermanently(req, res) {    
+    res.writeHead(301, {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Location' : '/top'
+    });
+    res.end('<!DOCTYPE html><html lang="jp"><body>' +
+            '<h1>新しいURLに移動しました</h1>' +
+            '<a href="/top">新しいほう</h1>' +
+            '</body></html>');
+}
+
+function handleUnauthorized(req, res) {
+    res.writeHead(401, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.end('<!DOCTYPE html><html lang="jp"><body>' +
+            '<h1>ログアウトしました</h1>' +
+            '<a href="/top">ログイン</h1>' +
+            '</body></html>');
+}
+
+function handleNotFound(req, res){
+    res.writeHead(404, {
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+    const message = "指定したURLが見つかりません。";
     res.end('status code :' + res.statusCode + " " +message);
 }
 
@@ -84,4 +100,3 @@ const port = 8000;
 server.listen(port, () => {
     console.info('Listening on ' + port);
 });
-
